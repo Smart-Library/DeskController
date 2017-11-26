07d90ab61069b044390550598c843b8ade798da7
erickadbay <erickadbay@gmail.com>
2017-11-25 20:34:01 -0500

    New ApiService to communicate with our cloud API

---
+++ tests/api_service_test.py
@@ -0,0 +1,16 @@
+from vcr_unittest import VCRTestCase
+from api_service import ApiService
+
+class TestApiService(VCRTestCase):
+
+    def test_create_desk_is_successful(self):
+        response = ApiService.create_desk(name = 'test desk')
+        json_response = response.json()
+
+        self.assertEqual(response.status_code, 201)
+        assert 'id' in json_response
+        self.assertEqual('test desk', json_response['name'])
+
+    def test_update_desk_occupied_status_is_successful(self):
+        response = ApiService.update_desk_occupied_status(id=1, occupied_status = True)
+        self.assertEqual(response.status_code, 204)
